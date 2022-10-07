fec92a54716fa9a828a374b6221a7ffcf1c00016
admont02 <admont02@ucm.es>
2022-10-07 18:34:23 +0200

    loading bar

--- src/boot.js
+++ src/boot.js
@@ -13,12 +13,55 @@ export default class Boot extends Phaser.Scene {
    * Carga de los assets del juego
    */
   preload() {
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0xBD0DBD, 0.8);
+    progressBox.fillRect(340, 270, 320, 50);
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(350, 280, 300 * value, 30);
+    });
+
     // Con setPath podemos establecer el prefijo que se añadirá a todos los load que aparecen a continuación
     this.load.setPath('assets/sprites/');
     this.load.image('platform', 'platform.png');
     this.load.image('base', 'base.png');
     this.load.image('star', 'star.png');
     this.load.image('player', 'player.png');
+
+     //TILEMAP
+    //  this.load.setPath('tilemap/');
+    //  this.load.tilemapTiledJSON('map1', 'level1.json');
+    //  this.load.image('patronesLevel1', 'tilesetForest.png');
+    //  this.load.tilemapTiledJSON('map2', 'level2.json');
+    //  this.load.image('patronesLevel2', 'tilesetbeach.png');
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+      x: width / 2 - 75,
+      y: height / 2 - 50,
+      text: 'LOADING...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0);
+
+  
   }
 
   /**
--- src/level.js
+++ src/level.js
@@ -16,6 +16,19 @@ export default class Level extends Phaser.Scene {
    */
   create() {
     this.player = new Player(this, 200, 300);
+    //this.createTileMap();
+  }
+  createTileMap() {
+    this.map = this.make.tilemap({
+      key: 'map',
+      tileWidth: 64,
+      tileHeight: 64
+    });
+    const tileset1 = this.map1.addTilesetImage('tilesetForest', 'patronesLevel1');
+    //this.backgroundLayer = this.map.createLayer('Background', tileset1);
+    this.groundLayer = this.map1.createLayer('Floor', tileset1);
+    this.physics.add.collider(this.player, this.groundLayer);
+    this.groundLayer.setCollisionBetween(0, 999);
 
   }
 }
