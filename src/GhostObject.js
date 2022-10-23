726182128d69c4682a6f137b358cae351daa3fc3
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2022-10-23 18:06:30 +0200

    cambios en moving object

---
+++ src/GhostObject.js
@@ -0,0 +1,22 @@
+
+ export default class GhostObject extends Phaser.GameObjects.Sprite {
+
+  constructor(scene, x, y, player) {
+    super(scene, x, y, 'star');
+
+
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this, true);
+
+    this.scene.physics.add.collider(this, player);
+    
+    this.player = player;
+  }
+
+  preUpdate(t, dt) {
+    super.preUpdate(t, dt);
+
+  }
+
+
+}
