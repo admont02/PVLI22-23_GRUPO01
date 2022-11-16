e21c00aef77adb71280a1579736f35596b93af3b
asegar01 <asegar01@ucm.es>
2022-11-16 17:27:05 +0100

    enemigos

--- src/Enemies/RangedEnemy.js
+++ src/Enemies/RangedEnemy.js
@@ -2,111 +2,103 @@ import Enemy from './enemy.js';
 import balaRangedEnemy from '../Objects/Bullets/balaRangedEnemy.js';//para poder instanciar balas enemigas
 
 export default class RangedEnemy extends Enemy {
-    /**
-     * Constructor de Goblin
-     * @param {Scene} scene Escena en la que aparece la estrella
-     * @param {number} x coordenada x
-     * @param {number} y coordenada y
-     */
-    
-      constructor(scene, x, y, imgKey, player,tiempoBala) {
-      //constructor que invoca a la clase enemy con scene con tamaño posicion , con una velocidad determinada y con la foto star
-      //falta poner el sprite que queramos
-          super(scene, x, y, 200, imgKey, player,20,150);
-          //instancia para poder modificar clase player desde aqui
-          this.setScale(1.5);
-          this.playerX = this.player.x;
-          this.playerY = this.player.y;
-          this.tiempoBala = 4000;
-          this.lastShot =0;
-        //aplicar escala en X e Y en BasicEnemy 
-        //  this.setScale(50,50);--> multiplicaba el tamaño que ya tiene por defecto x50
-        //sino pones nada mete el tamaño por defecto
-        
-      }
+  /**
+   * Constructor de Goblin
+   * @param {Scene} scene Escena en la que aparece la estrella
+   * @param {number} x coordenada x
+   * @param {number} y coordenada y
+   */
 
-      //crear metodo aquí que cada 4 segundos pongan 1 flecha en posicion player x y player y
+  constructor(scene, x, y, imgKey, player, tiempoBala) {
+    //constructor que invoca a la clase enemy con scene con tamaño posicion , con una velocidad determinada y con la foto star
+    //falta poner el sprite que queramos
+    super(scene, x, y, 200, imgKey, player, 20, 150);
+    //instancia para poder modificar clase player desde aqui
+    this.setScale(1.5);
+    this.playerX = this.player.x;
+    this.playerY = this.player.y;
+    this.tiempoBala = 4000;
+    this.lastShot = 0;
 
-      //permite llamar a la clase padre para la herencia
-      preUpdate(t, dt)
-      {
-        super.preUpdate(t, dt);
-        this.delta = t- this.lastShot;// t es tiempo que ha pasado de partida en milisegundos
-        if(this.delta>this.tiempoBala)
-        { //disparas cada 4 segundos
-          this.enemyShoot();
-          this.lastShot=t;
-        }
+    this.play('RangedEnemyDer', true);
+    //aplicar escala en X e Y en BasicEnemy
+    //  this.setScale(50,50);--> multiplicaba el tamaño que ya tiene por defecto x50
+    //sino pones nada mete el tamaño por defecto
 
-        if(this!==undefined)
-        {
-          this.animsRangedEnemy();
-          //velocidad 50
-          this.body.velocity.normalize().scale(50);
-  
-        }
-       
-  
-      } 
+  }
 
-    
-    //disparar enemigo en x e y a la pos del player
-    enemyShoot()
-    {
-      //creas la bala y le pasas el origen de disparo y la direccion que es la resta entre el destino y el origen
-      var BalaRangedEnemy = new balaRangedEnemy(this.scene, this.x, this.y, this.player.x-this.x, this.player.y- this.y,this.player);
+  //crear metodo aquí que cada 4 segundos pongan 1 flecha en posicion player x y player y
+
+  //permite llamar a la clase padre para la herencia
+  preUpdate(t, dt) {
+    super.preUpdate(t, dt);
+    this.delta = t - this.lastShot;// t es tiempo que ha pasado de partida en milisegundos
+    if (this.delta > this.tiempoBala) { //disparas cada 4 segundos
+      this.enemyShoot();
+      this.lastShot = t;
     }
-    animsRangedEnemy()
-      {
 
-        if(this!==undefined)
-        {
-                //si esta quieto
-            if (this.body.velocity.x === 0 && this.body.velocity.y === 0) 
-            {
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a la derecha (1,0)
-            else if (this.body.velocity.x > 0 && this.body.velocity.y === 0) 
-            {
-              console.log("ha");
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a la izq (-1,0)
-            else if (this.body.velocity.x < 0 && this.body.velocity.y === 0) {
-              this.play('RangedEnemyIzq', true);
-            }
-            //si va a la arriba (0,1)
-            else if (this.body.velocity.x === 0 && this.body.velocity.y > 0) {
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a abajo (0,-1)
-            else if (this.body.velocity.x === 0 && this.body.velocity.y < 0) {
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a la derecha arriba(1,1)
-            else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a la derecha abajo (1,-1)
-            else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
-              this.play('RangedEnemyDer', true);
-            }
-            //si va a la izq abajo (-1,-1)
-            else if (this.body.velocity.x < 0 && this.body.velocity.y <0) {
-              this.play('RangedEnemyIzq', true);
-            }
-            //si va a la izq arriba(-1,1)
-            else if (this.body.velocity.x < 0 && this.body.velocity.y > 0) {
-              this.play('RangedEnemyIzq', true);
-            }
-        }
-       
-      }
+    if (this !== undefined) {
+      this.animsRangedEnemy();
+      //velocidad 50
+      this.body.velocity.normalize().scale(50);
 
-    
-
-      
+    }
 
 
   }
+
+
+  //disparar enemigo en x e y a la pos del player
+  enemyShoot() {
+    //creas la bala y le pasas el origen de disparo y la direccion que es la resta entre el destino y el origen
+    var BalaRangedEnemy = new balaRangedEnemy(this.scene, this.x, this.y, this.player.x - this.x, this.player.y - this.y, this.player);
+  }
+  animsRangedEnemy() {
+
+    if (this !== undefined) {
+      if (this.body.velocity.x >= 0) this.setFlipX(0);
+      else this.setFlipX(1);
+
+      //     //si esta quieto
+      // if (this.body.velocity.x === 0 && this.body.velocity.y === 0)
+      // {
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a la derecha (1,0)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y === 0)
+      // {
+      //   console.log("ha");
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a la izq (-1,0)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y === 0) {
+      //   this.play('RangedEnemyIzq', true);
+      // }
+      // //si va a la arriba (0,1)
+      // else if (this.body.velocity.x === 0 && this.body.velocity.y > 0) {
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a abajo (0,-1)
+      // else if (this.body.velocity.x === 0 && this.body.velocity.y < 0) {
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a la derecha arriba(1,1)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a la derecha abajo (1,-1)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
+      //   this.play('RangedEnemyDer', true);
+      // }
+      // //si va a la izq abajo (-1,-1)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y <0) {
+      //   this.play('RangedEnemyIzq', true);
+      // }
+      // //si va a la izq arriba(-1,1)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y > 0) {
+      //   this.play('RangedEnemyIzq', true);
+      // }
+    }
+  }
+}
--- src/Enemies/StrongEnemy.js
+++ src/Enemies/StrongEnemy.js
@@ -1,90 +1,89 @@
 import Enemy from './enemy.js';
 
 export default class StrongEnemy extends Enemy {
-    /**
-     * Constructor de Goblin
-     * @param {Scene} scene Escena en la que aparece la estrella
-     * @param {number} x coordenada x
-     * @param {number} y coordenada y
-     */
-    
-      constructor(scene, x, y, imgKey, player) {
-      //constructor que invoca a la clase enemy con scene con tamaño posicion , con una velocidad determinada y con la foto star
-      //falta poner el sprite que queramos
-          super(scene, x, y, 50, imgKey, player, 40,150);
-        
-          //instancia para poder modificar clase player desde aqui
-         // this.hp = new HealthBar(scene, 10, 10, this);
-        //aplicar escala en X e Y en BasicEnemy 
-        //  this.setScale(50,50);--> multiplicaba el tamaño que ya tiene por defecto x50
-        //sino pones nada mete el tamaño por defecto
-        this.setScale(1);
+  /**
+   * Constructor de Goblin
+   * @param {Scene} scene Escena en la que aparece la estrella
+   * @param {number} x coordenada x
+   * @param {number} y coordenada y
+   */
 
-      }
+  constructor(scene, x, y, imgKey, player) {
+    //constructor que invoca a la clase enemy con scene con tamaño posicion , con una velocidad determinada y con la foto star
+    //falta poner el sprite que queramos
+    super(scene, x, y, 50, imgKey, player, 40, 150);
 
-      //permite llamar a la clase padre para la herencia
-      preUpdate(t, dt) {
-        super.preUpdate(t, dt);
-        // this.hp.bar.setX(this.x-this.scene.cameras.main._scrollX-this.width)
-        // this.hp.bar.setY(this.y-this.height)
-        //hacer animaciones
-        if(this!==undefined)
-        {
-          this.animsStrongEnemy();
-          //velocidad 50
-          this.body.velocity.normalize().scale(50);
-          // console.log(this.body.velocity.x+","+this.body.velocity.y);
-        }
-        
-      }
+    //instancia para poder modificar clase player desde aqui
+    // this.hp = new HealthBar(scene, 10, 10, this);
+    //aplicar escala en X e Y en BasicEnemy
+    //  this.setScale(50,50);--> multiplicaba el tamaño que ya tiene por defecto x50
+    //sino pones nada mete el tamaño por defecto
+    this.setScale(1);
 
+    this.play('StrongEnemyWalk', true);
+  }
 
-      animsStrongEnemy()
-      {
-        if(this!==undefined)
-        {
-             //si esta quieto
-        if (this.body.velocity.x === 0 && this.body.velocity.y === 0) 
-        {
-           this.play('StrongEnemyWalk', true);
-        }
-        //si va a la derecha (1,0)
-        else if (this.body.velocity.x > 0 && this.body.velocity.y === 0) 
-        {
-          console.log("ha");
-          this.play('StrongEnemyWalk', true);
-        }
-        //si va a la izq (-1,0)
-        else if (this.body.velocity.x < 0 && this.body.velocity.y === 0) {
-          this.play('StrongEnemyIzq', true);
-        }
-        //si va a la arriba (0,1)
-        else if (this.body.velocity.x === 0 && this.body.velocity.y > 0) {
-          this.play('StrongEnemyWalk', true);
-        }
-        //si va a abajo (0,-1)
-        else if (this.body.velocity.x === 0 && this.body.velocity.y < 0) {
-          this.play('StrongEnemyWalk', true);
-        }
-        //si va a la derecha arriba(1,1)
-        else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
-          this.play('StrongEnemyWalk', true);
-        }
-        //si va a la derecha abajo (1,-1)
-        else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
-          this.play('StrongEnemyWalk', true);
-        }
-        //si va a la izq abajo (-1,-1)
-        else if (this.body.velocity.x < 0 && this.body.velocity.y <0) {
-          this.play('StrongEnemyIzq', true);
-        }
-        //si va a la izq arriba(-1,1)
-        else if (this.body.velocity.x < 0 && this.body.velocity.y > 0) {
-          this.play('StrongEnemyIzq', true);
-        }
-        }
-       
-      }
-
+  //permite llamar a la clase padre para la herencia
+  preUpdate(t, dt) {
+    super.preUpdate(t, dt);
+    // this.hp.bar.setX(this.x-this.scene.cameras.main._scrollX-this.width)
+    // this.hp.bar.setY(this.y-this.height)
+    //hacer animaciones
+    if (this !== undefined) {
+      this.animsStrongEnemy();
+      //velocidad 50
+      this.body.velocity.normalize().scale(50);
+      // console.log(this.body.velocity.x+","+this.body.velocity.y);
+    }
 
   }
+
+
+  animsStrongEnemy() {
+    if (this !== undefined) {
+      if (this.body.velocity.x >= 0) this.setFlipX(0);
+      else this.setFlipX(1);
+
+      //si esta quieto
+      // if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a la derecha (1,0)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y === 0) {
+      //   console.log("ha");
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a la izq (-1,0)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y === 0) {
+      //   this.play('StrongEnemyIzq', true);
+      // }
+      // //si va a la arriba (0,1)
+      // else if (this.body.velocity.x === 0 && this.body.velocity.y > 0) {
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a abajo (0,-1)
+      // else if (this.body.velocity.x === 0 && this.body.velocity.y < 0) {
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a la derecha arriba(1,1)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a la derecha abajo (1,-1)
+      // else if (this.body.velocity.x > 0 && this.body.velocity.y > 0) {
+      //   this.play('StrongEnemyWalk', true);
+      // }
+      // //si va a la izq abajo (-1,-1)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y < 0) {
+      //   this.play('StrongEnemyIzq', true);
+      // }
+      // //si va a la izq arriba(-1,1)
+      // else if (this.body.velocity.x < 0 && this.body.velocity.y > 0) {
+      //   this.play('StrongEnemyIzq', true);
+      // }
+    }
+
+  }
+
+
+}
