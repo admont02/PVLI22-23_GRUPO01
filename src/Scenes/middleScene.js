b4791bb052268baeea72e7fd14286e8489abc9c9
asegar01 <asegar01@ucm.es>
2022-12-12 00:50:28 +0100

    avances

--- src/Scenes/middleScene.js
+++ src/Scenes/middleScene.js
@@ -18,6 +18,17 @@ export default class MiddleScene extends Phaser.Scene {
         this.text = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 100, 'You have finished the level').setInteractive();
         this.text.setScale(2);
 
+        this.player = this.add.image(this.scale.width / 2, this.scale.height / 2, 'cara');
+        this.player.setScale(2);
+        this.tweens.add({
+            targets: this.player,
+            y: this.player.y + 10,
+            duration: 1000,
+            ease: 'Power2',
+            repeat: -1,
+            yoyo: true
+        });
+
         this.playButton = this.add.text(this.scale.width / 2 - 150, this.scale.height / 2 + 100, 'Next level').setInteractive();
         this.playButton.setScale(3);
 
