526ea5cb0eb362331f89a5b2bad1163d636601ae
asegar01 <asegar01@ucm.es>
2022-12-15 00:14:31 +0100

    Avances 2
    Co-Authored-By: PabloGarciaMoreno <82375336+PabloGarciaMoreno@users.noreply.github.com>

--- src/Scenes/middleScene.js
+++ src/Scenes/middleScene.js
@@ -11,12 +11,26 @@ export default class MiddleScene extends Phaser.Scene {
     create(data) {
         this.text = data.text;
 
-        this.background = this.add.image(500, 250, 'morado');
+        this.background = this.add.image(500, 250, 'fondo');
         this.middleSceneText = this.add.text(this.scale.width / 2 - 250, this.scale.height / 5, this.text).setInteractive();
         this.middleSceneText.setScale(5);
 
-        this.text = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 100, 'You have finished the level').setInteractive();
-        this.text.setScale(2);
+        //habilidad 1, si pasas a la escena 2
+        if (this.mapName === 'mapLevel1') {
+            this.text = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 200, 'Level 1 completed').setInteractive();
+            this.text.setScale(2);
+            this.text2 = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 100, '-You have unlocked the dash!').setInteractive();
+            this.text2.setScale(2);
+        }
+
+         //habilidad 2
+        if (this.mapName === 'map2') {
+            this.text = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 200, 'Level 2 completed').setInteractive();
+            this.text.setScale(2);
+            this.text3 = this.add.text(this.scale.width / 2 - 250, this.scale.height / 2 - 100, '-You have unlocked the click damage!').setInteractive();
+            this.text3.setScale(2);
+        }
+
 
         this.player = this.add.image(this.scale.width / 2, this.scale.height / 2, 'cara');
         this.player.setScale(2);
